6087fc01bbd8e1e4d7e44f591f9e865df2390383
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-10-12 09:54:15 -0600

    communication

--- routes/apiRoutes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const noteRoutes = require('../apiRoutes/noteRoute');
-
-router.use(noteRoutes);
-
-
-module.exports =  router;
--- server.js
+++ server.js
@@ -1,16 +1,60 @@
-const express = require("express");
-const PORT = process.envPORT || 3001;
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
-app.use(express.static("public"));
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get('api/notes/:id', (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(notes[req.params.id]);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  let noteId = (notes.length + 1).toString();
+  newNote.id = noteId;
+  notes.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2));
+  res.json(notes);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+  let newID = 0;
+  
+  savedNotes = notes.filter(currNote => {
+      return currNote.id != noteID;
+  })
+  
+  for (currNote of savedNotes) {
+      currNote.id = newID.toString();
+      newID++;
+  }
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes, null, 2));
+  res.json(savedNotes);
+});
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
-  });
-
-  
+  console.log(`API server now on port ${PORT}`);
+});
